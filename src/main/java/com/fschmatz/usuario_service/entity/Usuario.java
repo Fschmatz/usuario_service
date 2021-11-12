57cc8f16cfba2617c0174f4e378e8f3eaebd3e6f
Fschmatz <fschmatz@hotmail.com>
2021-11-11 22:10:26 -0300

    v 0.0.3
    Added Eureka

--- src/main/java/com/fschmatz/usuario_service/entity/Usuario.java
+++ src/main/java/com/fschmatz/usuario_service/entity/Usuario.java
@@ -4,8 +4,6 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
 import java.util.ArrayList;
